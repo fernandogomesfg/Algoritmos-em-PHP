f2ac6c1719dd9bf445ec897d746bdb6965699683
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-08-21 07:37:59 +0200

    Update factorial.php

--- factorial/factorial.php
+++ factorial/factorial.php
@@ -1,10 +1,11 @@
+
 <?php
 
-    $num = 0;   //numero que sera calculado o factorial
+    $num = 10;   //numero que sera calculado o factorial
     $resut = 1; //variavel que ira albergar o resultado do factorial
     for ($i= $num; $i > 1 ; $i--) { 
         $resut*=$i;
     }
-    echo "O factorial de $num eh $resut";
+    echo "O factorial de $num é $resut";
 
 ?>
